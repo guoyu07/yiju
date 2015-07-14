cda9204cf265d8cfbacd3deb1d5ba7608ed67ea3
viking zhang <viking_zhang@apple.com>
2015-07-14 16:15:57 +0800

    add lyrics fetch for yiju

--- app/controllers/song.js
+++ app/controllers/song.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
-
+function htmlspecialchars_decode(str) {
+    return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&apos;/g, '\'');
+}
 export default Ember.Controller.extend({
 	comments : [
 		{title: 'i love'},
@@ -10,6 +12,14 @@ export default Ember.Controller.extend({
 		var title = this.get('song.title');
 		return title.length;
 	}.property('song.title'),
+	decodeLyrics: function() {
+		var lyrics = this.get('song.lyrics');
+		if (lyrics) {
+			return htmlspecialchars_decode(lyrics);
+		} else {
+			return '';
+		}
+	}.property('song.lyrics'),
 	niceComments : Ember.computed.map('comments', function(comment, index) {
 		return {title: comment.title.toUpperCase()};
 	}),
